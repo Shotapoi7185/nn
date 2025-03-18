49226d1c67f4c5e0761f48674cef54b8426dcf30
Shotapoi7185 <shotapoi1412@gmail.com>
2025-03-19 03:59:03 +0700

    asd

--- app.py
+++ app.py
@@ -12,55 +12,15 @@ import tensorflow as tf
 # สร้างแอป Flask
 app = Flask(__name__)
 tf.config.set_visible_devices([], 'GPU')
-knn = joblib.load('knn_model.pkl')
-svm = joblib.load('svm_model.pkl')
 nn = load_model('best_model.h5')
 # สร้าง route สำหรับ API
 CORS(app,origins="*")
 @app.route('/')
 def hello_world():
-    return jsonify(message=[0,1,2,3])
+    return jsonify(message=[0])
 
 # กำหนด route สำหรับทำนาย (predict)
-@app.route('/predict_knn', methods=['POST'])
-def predict_knn():
-    input_data = None
-    try:
-        data = request.get_json()
-        
-        if data is None:
-            return jsonify({'error': 'Invalid JSON format or no data provided'}), 400
-        
-        if 'input' not in data:
-            return jsonify({'error': 'No input data provided'}), 400
-        
-        input_data = np.array(data['input']).reshape(1, -1)
-        prediction = knn.predict(input_data)
-        print(f"Input data: {input_data}")
-        print(f"Prediction: {prediction}")
-        print(prediction)
-        return jsonify(prediction=prediction.tolist())
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
-
-@app.route('/predict_svm', methods=['POST'])
-def predict_svm():
-    try:
-        data = request.get_json()
-
-        if 'input' not in data:
-            return jsonify({'error': 'No input data provided'}), 400
-        
-        input_data = np.array(data['input']).reshape(1, -1)
-        
-        prediction = svm.predict(input_data)
-
-        return jsonify(prediction=prediction.tolist())
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
-    
 @app.route('/predict_nn', methods=['POST'])
 def predict_nn():
     try:
